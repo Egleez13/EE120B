de09028ee4ed25ed4480f057350bb03be9a26869
Emilio Gonzalez <egonz055@wch136-03.cs.ucr.edu>
2020-01-12 17:54:47 -0800

    Modiffied

--- sample_1/source/main.c
+++ sample_1/source/main.c
@@ -18,12 +18,19 @@ DDRA=0x00;
 DDRB=0xFF;
 PINA=0xFF;
 PORTB=0x00;
-unsigned char PenA0=(PINA&0xFE);
-unsigned char PenA1=(PINA&0xFD);
+unsigned char g_sensorA0 = 0x00; // B
+unsigned char l_sensorA1 = 0x00; // A
 
     /* Insert your solution below */
     while (1) {
+	if (g_sensorA0 == 0x00 && l_sensorA1 == 0x01)
+	{
+		PORTB = 0x01;
+	}
 
+	else {
+		PORTB = 0x00;
+	} 
     }
     return 1;
 }
