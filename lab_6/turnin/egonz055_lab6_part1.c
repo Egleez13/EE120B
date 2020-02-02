23e2d0500058acc3c159c58b2b7072abe76caed5
Emilio Gonzalez <egonz055@wch136-03.cs.ucr.edu>
2020-02-01 23:10:36 -0800

    files

--- lab_6/turnin/egonz055_lab6_part1.c
+++ lab_6/turnin/egonz055_lab6_part1.c
@@ -13,114 +13,139 @@
 #include "simAVRHeader.h"
 #endif
 
+#define A0 (~PINA&0x01)
+
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
 
+
+
 void TimerOn() {
-TCCR1B = 0x0B;
-OCR1A = 125;
-TIMSK1 = 0x02;
-TCNT1 = 0;
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
 
-_avr_timer_cntcurr = _avr_timer_M;
+	_avr_timer_cntcurr = _avr_timer_M;
 
-SREG |= 0x80;
+	SREG |= 0x80;
 }
 
 void TimerOff() {
-TCCR1B = 0x00;
+	TCCR1B = 0x00;
 }
 
 void TimerISR() {
-TimerFlag = 1;
+	TimerFlag = 1;
 }
 
 ISR(TIMER1_COMPA_vect) {
-_avr_timer_cntcurr--;
-if (_avr_timer_cntcurr == 0) {
-TimerISR();
-_avr_timer_cntcurr = _avr_timer_M;
-}
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
 }
 
-enum states {start, stateOne, stateTwo, stateThree} state;
-
-unsigned char led = 0x00;
+enum states {start, wait, stateOne, stateTwo, stateThree, reset} state; 
 
 void Tick() {
-PORTB = ~PINB;
+	PORTB = ~PINB;
+	PINA =	~PORTA;
 
-switch (state) { //transitions
-case start:
-state = stateOne;
-break;
+	switch (state) { //transitions
+		case start:
+		state = wait;
+		break;
+		
+		case wait:
+			if ((A0) && (PORTB == 0x06))
+			{
+				state = stateOne;
+			}
+			if ((A0) && (PORTB == 0x02))
+			{
+				state = stateTwo;
+			}
+			if ((A0) && (PORTB == 0x04))
+			{
+				state = stateThree;
+			}
+		break;
+		
+		case stateOne:
+			state = reset;
+		break;
 
-case stateOne:
-state = stateTwo;
-break;
+		case stateTwo:
+			state = reset;
+		break;
 
-case stateTwo:
-state = stateThree;
-break;
+		case stateThree:
+			state = reset;
+		break;
+		
+		case reset:
+			state = start;
+		break;
 
-case stateThree:
-state = stateOne;
-break;
+		default:
+		state = start;
+		break;
+	}
 
-default:
-state = start;
-break;
-}
+	switch (state){ //actions
+		case start:
+			PORTB = 0x07;
+		break;
+		
+		case wait:
+			PORTB = 0x01;
+			PORTB = 0x02;
+			PORTB = 0x04;
 
-switch (state){ //actions
-case start:
-//led = led & 0x00;
-PORTB = 0x07;
-break;
+		case stateOne:
+			PORTB = 0x01;
+		break;
 
-case stateOne:
-//led = led & 0x06;
-PORTB = 0x01;
-break;
+		case stateTwo:
+			PORTB = 0x02;
+		break;
 
-case stateTwo:
-//led = led & 0x05;
-PORTB = 0x02;
-break;
+		case stateThree:
+			PORTB = 0x04;
+		break;
 
-case stateThree:
-//led = led & 0x03;
-PORTB = 0x04;
-break;
-
-default:
-state = start;
-break;
-}
+		default:
+			state = start;
+		break;
+	}
 
 }
 
 void TimerSet(unsigned long M) {
-_avr_timer_M = M;
-_avr_timer_cntcurr = _avr_timer_M;
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
 }
 
 int main(void)
 {
-DDRB = 0xFF; PORTB = 0x00;
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
 
-state = start;
+	state = start;
 
-TimerSet(100);
-TimerOn();
+	TimerSet(300);
+	TimerOn();
 
-while(1)
-{
-Tick();
-//led = ~led;
-//PORTB = led;
-while(!TimerFlag){}
-TimerFlag = 0;
+	while(1)
+	{
+		Tick();
+		while(!TimerFlag){}
+		TimerFlag = 0;
+	}
 }
-}
+
+//add wait state for each one and add a reset
+//
