a08bd5da148f808f6cdf49dcd8c207c031871073
Emilio Gonzalez <egonz055@wch136-03.cs.ucr.edu>
2020-02-09 15:53:12 -0800

    files

---
+++ lab_8/turnin/egonz055_lab8_part2.c
@@ -0,0 +1,39 @@
+/*	Author: egonz055
+ *  Partner(s) Name: Leo
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned short num1;
+unsigned char num2;
+unsigned char num3;
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) (1 << ADATE);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0X00;
+	DDRD = 0X03; PORTD = 0x00;
+
+	ADC_init();
+	
+    while (1) {
+	num1 = ADC;
+	num2 = (char)num1;
+	num3 = (char)(num1 >> 8);
+	PORTB = num2;
+	PORTD = num3;
+    }
+    return 1;
+}
